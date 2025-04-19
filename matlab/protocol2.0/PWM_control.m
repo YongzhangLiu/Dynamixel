045a88cb16124f99bdf0699b2846f06cbf39ed19
RRC <yz.dennis.liu@gmail.com>
2025-04-19 16:00:20 +0800

    Basic PWM Control

---
+++ matlab/protocol2.0/PWM_control.m
@@ -0,0 +1,136 @@
+clc;
+clear all;
+
+% Includes
+lib_name = '';
+if strcmp(computer, 'PCWIN')
+  lib_name = 'dxl_x86_c';
+elseif strcmp(computer, 'PCWIN64')
+  lib_name = 'dxl_x64_c';
+elseif strcmp(computer, 'GLNX86')
+  lib_name = 'libdxl_x86_c';
+elseif strcmp(computer, 'GLNXA64')
+  lib_name = 'libdxl_x64_c';
+elseif strcmp(computer, 'MACI64')
+  lib_name = 'libdxl_mac_c';
+end
+% Load Libraries
+if ~libisloaded(lib_name)
+    [notfound, warnings] = loadlibrary(lib_name, 'dynamixel_sdk.h', 'addheader', 'port_handler.h', 'addheader', 'packet_handler.h');
+end
+
+
+% General config
+PROTOCOL_VERSION            = 2.0;          
+DXL_ID                      = 2; % -----------------------------------[CHANGED]
+DEVICENAME                  = 'COM5';  % -----------------------------[CHANGED]
+BAUDRATE                    = 4500000; % 4.5Mbps, 7 baudrate ---------[CHECK]
+
+% Common Control Table Address and Data 
+ADDR_TORQUE_ENABLE          = 64;
+ADDR_GOAL_POSITION          = 116;
+ADDR_GOAL_PWM               = 100;
+ADDR_PRESENT_POSITION       = 132;
+
+ADDR_OPERATING_MODE         = 11;          
+OPERATING_MODE              = 16;            % value for operating mode for PWM control                                
+TORQUE_ENABLE               = 1;            % Value for enabling the torque
+TORQUE_DISABLE              = 0;            % Value for disabling the torque
+DXL_MOVING_STATUS_THRESHOLD = 20;           % Dynamixel moving status threshold
+
+ESC_CHARACTER               = 'e';          % Key for escaping loop
+
+COMM_SUCCESS                = 0;            % Communication Success result value
+COMM_TX_FAIL                = -1001;        % Communication Tx Failed
+
+% Initializations
+    % Initialize PortHandler Structs
+    % Set the port path
+    % Get methods and members of PortHandlerLinux or PortHandlerWindows
+port_num = portHandler(DEVICENAME);
+    % Initialize PacketHandler Structs
+packetHandler();
+    % Variables initialization
+index = 1;
+dxl_comm_result = COMM_TX_FAIL;           % Communication result
+dxl_goal_pwm = 0;                           % Goal pwm (-855-855) ------[PWM]
+
+dxl_error = 0;                              % Dynamixel error
+dxl_present_position = 0;                   % Present position
+
+
+% Open port
+if (openPort(port_num))
+    fprintf('Succeeded to open the port!\n');
+else
+    unloadlibrary(lib_name);
+    fprintf('Failed to open the port!\n');
+    input('Press any key to terminate...\n');
+    return;
+end
+
+% Set port baudrate
+if (setBaudRate(port_num, BAUDRATE))
+    fprintf('Succeeded to change the baudrate!\n');
+else
+    unloadlibrary(lib_name);
+    fprintf('Failed to change the baudrate!\n');
+    input('Press any key to terminate...\n');
+    return;
+end
+
+% Set operating mode
+write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_OPERATING_MODE, OPERATING_MODE);
+if check_comm_result(port_num, PROTOCOL_VERSION) == true
+    fprintf('Operating Mode successfully set \n');
+end
+
+% Enable Dynamixel Torque
+write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE);
+if check_comm_result(port_num, PROTOCOL_VERSION) == true
+    fprintf('Dynamixel has been successfully connected \n');
+end
+
+
+while 1
+    if input('Press any key to continue! (or input e to quit!)\n', 's') == ESC_CHARACTER
+        break;
+    end
+
+    % Write goal position
+    dxl_goal_pwm = input('Input PWM Value\n');
+    write2ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_GOAL_PWM, typecast(int16(dxl_goal_pwm), 'uint16'));
+    check_comm_result(port_num, PROTOCOL_VERSION);
+
+    % Read present position
+    dxl_present_position = read4ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_PRESENT_POSITION);
+    check_comm_result(port_num, PROTOCOL_VERSION);
+end
+
+
+% Disable Dynamixel Torque
+write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE);
+check_comm_result(port_num, PROTOCOL_VERSION);
+
+% Close port
+closePort(port_num);
+
+% Unload Library
+unloadlibrary(lib_name);
+
+close all;
+clear all;
+
+% Functions
+function result = check_comm_result(port_num, PROTOCOL_VERSION)
+    dxl_comm_result = getLastTxRxResult(port_num, PROTOCOL_VERSION);
+    dxl_error = getLastRxPacketError(port_num, PROTOCOL_VERSION);
+    result = true;
+    if dxl_comm_result ~= 0 % 0:=COMM_SUCCESS
+        fprintf('%s\n', getTxRxResult(PROTOCOL_VERSION, dxl_comm_result));
+        result = false;
+    elseif dxl_error ~= 0
+        fprintf('%s\n', getRxPacketError(PROTOCOL_VERSION, dxl_error));
+        result = false;
+    end
+end
